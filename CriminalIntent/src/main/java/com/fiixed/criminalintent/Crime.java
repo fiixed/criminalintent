7eb043f7dfa261781108813edf23fa9eb44392da
Andrew Bell <fiixed@gmail.com>
2013-11-20 11:58:41 -0600

    added Crime and CrimeFragment classes, added fragment_crime.xml

---
+++ CriminalIntent/src/main/java/com/fiixed/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.fiixed.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by abell on 11/20/13.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
