c55e5f8f91dec2e579ca64ce923ecf3664d6c5c0
Andrew Bell <fiixed@gmail.com>
2013-12-22 15:32:52 -0600

    viewpager setup

--- CriminalIntent/src/main/java/com/fiixed/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/main/java/com/fiixed/criminalintent/CrimeListFragment.java
@@ -36,8 +36,8 @@ public class CrimeListFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 
-        //Start CrimeActivity
-        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        //Start CrimePagerActivity
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
---
+++ CriminalIntent/src/main/java/com/fiixed/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,71 @@
+package com.fiixed.criminalintent;
+
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by abell on 12/22/13.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int i) {
+                Crime crime = mCrimes.get(i);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                Crime crime = mCrimes.get(i);
+                if(crime.getTitle() != null) {
+                    setTitle(crime.getTitle());
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        UUID crimeId = (UUID)getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if(mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}
