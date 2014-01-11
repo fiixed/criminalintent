e381d442b36d2cf688b964df73704d0e2122f51c
Andrew Bell <fiixed@gmail.com>
2014-01-12 10:34:05 +1100

    added actionbar and removed dummy data

--- CriminalIntent/src/main/java/com/fiixed/criminalintent/CrimeLab.java
+++ CriminalIntent/src/main/java/com/fiixed/criminalintent/CrimeLab.java
@@ -17,12 +17,6 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
-        for(int i = 0; i < 100; i++) {
-            Crime c = new Crime();
-            c.setTitle("Crime # " + i);
-            c.setSolved(i % 2 == 0);  //Every other one
-            mCrimes.add(c);
-        }
     }
 
     public static CrimeLab get(Context c) {
@@ -30,6 +24,10 @@ public class CrimeLab {
            sCrimeLab = new CrimeLab(c.getApplicationContext());
         }
         return sCrimeLab;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public ArrayList<Crime> getCrimes() {
